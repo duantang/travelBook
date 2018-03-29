e046670cbc95b743bc440a96d3cb57294c896155
Kevin Schwarz <duantang@gmail.com>
2018-03-29 16:15:20 +0200

    Initial Commit

---
+++ travelBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travelBook
+//
+//  Created by Kevin Schwarz on 29.03.18.
+//  Copyright © 2018 KSARC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
